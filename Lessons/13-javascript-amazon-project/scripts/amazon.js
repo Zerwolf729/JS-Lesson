85cd34f4ae029d2626aedc3979157d0e8ee91e6d
Samuel <samueljuliank29@gmail.com>
2025-05-11 18:48:24 +0800

    Make the Checkout More Interactive

--- Lessons/13-javascript-amazon-project/scripts/amazon.js
+++ Lessons/13-javascript-amazon-project/scripts/amazon.js
@@ -1,3 +1,7 @@
+import {cart, addToCart } from '../data/cart.js';
+import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -41,7 +45,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -56,8 +60,11 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid')
 .innerHTML = productsHTML;
 
+const addedMessageTimeouts = {};
+
 document.querySelectorAll('.js-add-to-cart')
 .forEach((button) => {
+
   button.addEventListener('click', () => {
     //const productId = button.dataset.productId;
     const {productId} = button.dataset;
@@ -87,11 +94,31 @@ document.querySelectorAll('.js-add-to-cart')
     let cartQuantity = 0;
     cart.forEach((item) => {
       cartQuantity += item.quantity;
-    })
+    });
 
     document.querySelector('.js-cart-quantity')
     .innerHTML = cartQuantity;
+
+    const addedMessage = document.querySelector(
+      `.js-added-to-cart-${productId}`
+    );
+
+    addedMessage.classList.add('added-to-cart-visible');
+
+    const previousTimeoutId = addedMessageTimeouts[productId];
+
+    if (previousTimeoutId) {
+      clearTimeout(previousTimeoutId);
+    }
+
+    const timeoutId = setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
+
+    addedMessageTimeouts[productId] = timeoutId;
   });
 });
+
+
 
 
